9a54a50c45a75bfbd8b7de32a32ae8ea9439af80
jry <ijry@qq.com>
2025-12-23 01:26:02 +0800

    add: 新增table组件uni-app-x版本

--- uni_modules/uv-plus/components/up-th/up-th.vue
+++ uni_modules/uv-plus/components/up-th/up-th.vue
@@ -1,5 +1,5 @@
 <template>
-	<view class="up-td" :style="[tdStyle]">
+	<view class="up-th" :style="[thStyle]">
 		<slot></slot>
 	</view>
 </template>
@@ -18,36 +18,18 @@
 	 * @example
 	 */
 	export default {
-		name: 'up-td',
+		name: 'up-th',
 		mixins: [mpMixin, mixin, props],
 		props: {
 			// 宽度，百分比或者具体带单位的值，如30%， 200rpx等，一般使用百分比
 			width: {
 				type: [String],
-				default: 'auto'
-			},
-			textAlign: {
-				type: String,
-				default: ''
-			},
-			fontSize: {
-				type: String,
-				default: ''
-			},
-			borderColor: {
-				type: String,
-				default: ''
-			},
-			color: {
-				type: String,
 				default: ''
 			}
 		},
 		data() {
 			return {
-				tdStyle: {
-					
-				}
+				thStyle: {}
 			}
 		},
 		created() {
@@ -58,41 +40,27 @@
 			if (this.parent) {
 				// 将父组件的相关参数，合并到本组件
 				let style = {};
-				if (this.width != "auto") style.flex = `0 0 ${this.width}`;
+				if (this.width) style.flex = `0 0 ${this.width}`;
 				style.textAlign = this.parent.align;
-				style.fontSize = addUnit(this.parent.fontSize);
 				style.padding = this.parent.padding;
 				style.borderBottom = `solid 1px ${this.parent.borderColor}`;
 				style.borderRight = `solid 1px ${this.parent.borderColor}`;
-				style.color = this.parent.color;
-				if (this.textAlign != '') {
-					style.textAlign = this.textAlign;
-				}
-				if (this.fontSize != '') {
-					style.fontSize = this.fontSize;
-				}
-				if (this.borderColor != '') {
-					style.borderColor = this.borderColor;
-				}
-				if (this.color != '') {
-					style.color = this.color;
-				}
-				this.tdStyle = style;
+				Object.assign(style, this.parent.thStyle);
+				this.thStyle = style;
 			}
 		}
 	}
 </script>
 
 <style lang="scss" scoped>
-	.up-td {
+	.up-th {
 		@include flex;
 		flex-direction: column;
 		flex: 1;
 		justify-content: center;
-		font-size: 14px;
-		color: $up-content-color;
-		align-self: stretch;
-		box-sizing: border-box;
-		height: 100%;
+		font-size: 28rpx;
+		color: $up-main-color;
+		font-weight: bold;
+		background-color: rgb(245, 246, 248);
 	}
 </style>
